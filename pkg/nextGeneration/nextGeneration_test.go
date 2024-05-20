41a5e4dc4d335545cdd8edbed64fddee54442223
RaghavZepto <int-raghav.doda@zeptonow.com>
2024-05-20 12:34:58 +0530

    test: Added a test to ensure NextGeneration's return type is as required

--- pkg/nextGeneration/nextGeneration_test.go
+++ pkg/nextGeneration/nextGeneration_test.go
@@ -7,11 +7,21 @@ import (
 
 func Test_NextGeneration(t *testing.T){
 	var A [3][3]int = [3][3]int{
-		{1,2,3},
-		{1,2,3},
-		{1,2,3},
+		{1,1,1},
+		{0,0,0},
+		{0,0,0},
 	}
 	t.Run("Test to ensure NewGeneration doesnot Panic", func(t *testing.T){
 		assert.NotPanics(t,func(){NextGeneration(A)})
+	})
+
+	var expected [3][3]int = [3][3] int{
+		{0,0,0},
+		{0,1,0},
+		{0,0,0},
+	}
+	got := NextGeneration(A)
+	t.Run("Test added to ensure the NextGeneration function produces the correct next generation", func(t *testing.T){
+		assert.IsType(t,expected,got)
 	})
 }
